9adc5ea71cf43fbbf43e900f7ee318ba6c167ab8
Suren Kulasegaram <surenkula24@gmail.com>
2026-03-25 19:03:29 -0400

    initial commit

---
+++ Assignments/a3/includes/travel-config.inc.php
@@ -0,0 +1,14 @@
+
+<?php
+
+define('DBHOST', 'localhost');
+define('DBNAME', 'name here');
+define('DBUSER', 'testuser');
+define('DBPASS', 'mypassword');
+//define('DBCONNSTRING','sqlite:./art.db');
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+
+
+
+?>
+
